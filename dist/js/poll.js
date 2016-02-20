9683a26426733c2b4801d5784e000c1e4361a9b4
Josh Bivens <hotbioform@gmail.com>
2016-02-19 20:34:31 -0700

    Reorganize data

--- dist/js/poll.js
+++ dist/js/poll.js
@@ -1,17 +1,20 @@
 new Vue({
   el: "#app",
   data: {
-    title: "What's the best way to spend your time?",
-    options: [
-      { id: "opt_1", text: 'Sleep' },
-      { id: "opt_2", text: 'Develop' }
-    ]
+    poll: {
+      title: "What's the best way to spend your time?",
+      options: [
+        { id: "opt_1", text: 'Sleep' },
+        { id: "opt_2", text: 'Develop' }
+      ]
+    }
   },
   methods: {
     addOption: function() {
       var options = document.getElementById("options");
       var newOption = document.createElement("input");
       newOption.setAttribute("type", "text");
+      newOption.setAttribute("v-model", "options.text");
       options.appendChild(newOption);
     }
   }
