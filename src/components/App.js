b4bfa743aa20d0f10b97c7cd0e1f0516e2b3b9b9
Josh Bivens <hotbioform@gmail.com>
2017-06-22 14:03:37 -0600

    Create folders

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../logo.svg';
+import '../css/App.css';
 import TestComponent from './Test.js';
 
 class App extends Component {
